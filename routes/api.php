80d58814c726dd588f4f303de6a2d0016ef15c8d
garayot <garayot@gmail.com>
2023-03-25 23:30:07 +0800

    Added update function

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('/carousel', function () {
-//     return 'Hello World';
-// });
-
 
 Route::get('/carousel', [CarouselItemsController::class, 'index']);
 
